148993f03f55561e0ca5803b6e17c91bd4013f84
Manoj Krishnappa <manojkr@infinite.com>
2025-07-01 11:05:43 +0530

    Final changes

---
+++ Csharp/Assignments/Assignment 1/Assignment 1/Porgram2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Assignment_1
+{
+    class Porgram2
+    {
+        static void Main()
+        {
+            /*fdsffd*/
+            Console.Write("Input a number: ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            if (num >= 0)
+                Console.WriteLine($"{num} is a positive number.");
+            else
+                Console.WriteLine($"{num} is a negative number.");
+            Console.ReadLine();
+        }
+
+    }
+}
